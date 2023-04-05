4b59a52f0e75fa07efda239b6de4f7e5287a37d5
Jose Lo <joalloce@gmail.com>
2023-04-05 17:31:07 -0600

    add signbit check

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"time"
@@ -15,8 +16,10 @@ func main() {
 	} else {
 		result, stopError := parseFloats(args)
 		if !stopError {
-			fmt.Println(result)
-			fmt.Println(len(result))
+			fmt.Println(result)      //
+			fmt.Println(len(result)) //
+			charts(result)
+			// create chart
 		} else {
 			fmt.Println("something went bad")
 		}
@@ -39,18 +42,45 @@ func yearbar() {
 	bar += " " + fmt.Sprintf("%.2f", porcentage) + "%"
 
 	fmt.Println(bar)
+	fmt.Println(time.Now().Date())
 }
 
 func parseFloats(args []string) ([]float64, bool) {
 	stopError := false
 	result := make([]float64, len(args))
+
 	for i, arg := range args {
 		if number, err := strconv.ParseFloat(arg, 64); err == nil {
+			sign := math.Signbit(number) // check if negative
+
+			if sign {
+				stopError = true
+				break
+			}
+
 			result[i] = number
 		} else {
-			stopError = true
+			stopError = true // error parsing to float64
 			break
 		}
 	}
+
 	return result, stopError
+}
+
+func charts(args []float64) {
+	max := max(args)
+	fmt.Println(max)
+}
+
+func max(args []float64) float64 {
+	max := args[0]
+
+	for _, number := range args {
+		if number > max {
+			max = number
+		}
+	}
+
+	return max
 }
